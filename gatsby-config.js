f82693d228ca8b6bd77c472be3d8cfb171bffba1
Lucas Henrique <lucasheriques@gmail.com>
2021-06-13 18:59:48 -0300

    Adding GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,14 +38,14 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /assets/, // See below to configure properly
-        },
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-react-svg",
+    //   options: {
+    //     rule: {
+    //       include: /assets/, // See below to configure properly
+    //     },
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -58,9 +58,37 @@ module.exports = {
         icon: `src/assets/favicon.png`,
       },
     },
+    // {
+    //   resolve: `gatsby-plugin-netlify-cms`,
+    //   options: {},
+    // },
     {
-      resolve: `gatsby-plugin-netlify-cms`,
-      options: {},
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-PFT28LL",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+      },
     },
   ],
 };
